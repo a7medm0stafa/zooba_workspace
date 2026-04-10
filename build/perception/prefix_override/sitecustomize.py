922575896a79d9ca495687df78f9e9b2c738ed7d
Ahmed <ahm.mostafa03@gmail.com>
2026-04-10 15:20:27 +0200

    chore: update workspace paths and initialize gitignore

--- build/perception/prefix_override/sitecustomize.py
+++ build/perception/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ahmed/autonomous_ws/install/perception'
+    sys.prefix = sys.exec_prefix = '/home/ahmed/zooba_workspace/install/perception'
