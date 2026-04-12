4c85b4f6a028346a454b60ddf883edc1c3909b12
Ahmed <ahm.mostafa03@gmail.com>
2026-04-12 14:13:31 +0200

    hardware testing

--- src/low_level_controller/launch/low_level_controller.launch.py
+++ src/low_level_controller/launch/low_level_controller.launch.py
@@ -26,7 +26,7 @@ def generate_launch_description():
         description='Serial baud rate'
     )
     max_velocity_arg = DeclareLaunchArgument(
-        'max_velocity', default_value='1.0',
+        'max_velocity', default_value='3.0',
         description='Maximum velocity in m/s'
     )
     wheel_radius_arg = DeclareLaunchArgument(
